da17e27fe2a234a0bad1dd0089bc7dbc574a71ee
chang yeon cho <ckddus1130@gmail.com>
2021-01-02 14:32:50 +0900

    JS: Navbar

---
+++ main.js
@@ -0,0 +1,23 @@
+'use strict'
+
+// Make navbar transparent when it is on the top
+const navbar = document.querySelector('#navbar');
+const navbarHeight = navbar.getBoundingClientRect().height;
+
+document.addEventListener('scroll', ()=> {
+  //console.log(`navbarHeight: ${navbarHeight}`); // 100.9375
+  if(window.scrollY > navbarHeight){
+    navbar.classList.add('navbar--dark');
+  }else{
+    navbar.classList.remove('navbar--dark');
+  }
+});
+
+
+
+
+
+
+
+
+
