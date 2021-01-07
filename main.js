ccc550ddedbf53ed754b7f59118ece6a855e5a01
chang yeon cho <ckddus1130@gmail.com>
2021-01-07 15:36:43 +0900

    JS: navbar menu toggle & project UI

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ document.addEventListener('scroll', ()=> {
   }
 });
 
+
 // Handle scrolling when tapping on the navbar menu
 const navbarMenu = document.querySelector('.navbar__menu');
 navbarMenu.addEventListener('click', (event) => {
@@ -22,8 +23,15 @@ navbarMenu.addEventListener('click', (event) => {
   if(link == null) {
     return;
   }else  {
+    navbarMenu.classList.remove('open');
     scrollIntoView(link);
   }
+});
+
+//Navbar toggle button for small screen 일때 
+const navbarToggleBtn = document.querySelector('.navbar__toggle-btn');
+navbarToggleBtn.addEventListener('click', () => {
+  navbarMenu.classList.toggle('open');
 });
 
 // Handle contact button on home
